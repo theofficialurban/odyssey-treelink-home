9d1acfe26db0c3c340a40be9f286fa02e307c770
𝔘𝔯𝔟𝔞𝔫 <theofficialurban@protonmail.com>
2025-08-14 02:53:04 -0400

    initial

--- src/siteConfig.ts
+++ src/siteConfig.ts
@@ -1,7 +1,10 @@
+import type { ComponentInstance, Props } from "astro";
 import siteConfigJson from "./siteConfig.json";
+import { YouTube, Tweet, LinkPreview } from "astro-embed";
 
 export interface SiteConfiguration {
   name: string;
+  subName?: string;
   bio: string;
   profilePicture: string;
   url: string;
@@ -16,12 +19,24 @@ interface IconLink {
   url: string;
 }
 
-interface CustomLink {
+export interface CustomLink {
   id: string;
   title: string;
   url: string;
+  className?: string;
+  customHtml?: string;
+  isModel: boolean | EmbedModel;
 }
-
+export interface EmbedModel {
+  embed: string;
+  url: string;
+}
 export const SITE: SiteConfiguration = {
   ...siteConfigJson,
+};
+
+export const EMBEDS = {
+  youtube: YouTube,
+  link: LinkPreview,
+  tweet: Tweet,
 };
